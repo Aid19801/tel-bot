f3014ad01c4a0deac5fb2ae95ad0a2c81d272417
Aid Thompson <adrian@nymtech.net>
2021-11-14 07:52:42 +0000

    1st commit

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+// export const BOT_USERNAME = 'tasker_871264_bot';
+// export const BOT_LINK = 't.me/tasker_871264_bot';
+// export const BOT_TOKEN = '2108176466:AAFqDzjP5hMVKzyHnc-ruGhSqyoNxyfo6f4';
+// export const TELEGRAM_API = 'https://api.telegram.org/bot';
