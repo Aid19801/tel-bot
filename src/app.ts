f2c5b2f664d607a4182c683f216ddfd8e150ddbd
Aid Thompson <adrian@nymtech.net>
2021-11-15 11:14:36 +0000

    Added bot logic to express server for Heroku

--- src/app.ts
+++ src/app.ts
@@ -1,81 +1,78 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-require('dotenv').config();
-import { Context, Markup, Telegraf, Telegram } from 'telegraf';
-import { Update } from 'typegram';
-import express from 'express';
+require("dotenv").config();
+import { Context, Markup, Telegraf, Telegram } from "telegraf";
+import { Update } from "typegram";
+import express from "express";
 
 const expressApp = express();
 const port = process.env.PORT || 3001;
 
-expressApp.get('/', (req: any, res: any) => {
-  res.send('Hello World!')
-})
+expressApp.get("/", (req: any, res: any) => {
+  res.send("Hello World!");
+});
 
 expressApp.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
 
-const token: string = process.env.BOT_TOKEN as string;
-const telegram: Telegram = new Telegram(token);
-const bot: Telegraf<Context<Update>> = new Telegraf(token);
-const chatId: string = process.env.CHAT_ID as string;
+  const token: string = process.env.BOT_TOKEN as string;
+  const telegram: Telegram = new Telegram(token);
+  const bot: Telegraf<Context<Update>> = new Telegraf(token);
+  const chatId: string = process.env.CHAT_ID as string;
 
-console.log("Nym Bot starting...");
-
-// bot.hears(/./, (ctx) => ctx.reply('Hello'))
-
-bot.start((ctx) => {
-  ctx.reply('Hello ' + ctx.from.first_name + '!');
-});
-
-bot.help((ctx) => {
-  ctx.reply('Send /start to receive a greeting');
-//   ctx.reply('Send /start to receive a greeting');
-//   ctx.reply('Send /keyboard to receive a message with a keyboard');
-//   ctx.reply('Send /quit to stop the bot');
-});
-
-bot.command('quit', (ctx) => {
-  // Explicit usage
-  ctx.telegram.leaveChat(ctx.message.chat.id);
-
-  // Context shortcut
-  ctx.leaveChat();
-});
-
-bot.command('keyboard', (ctx) => {
+  bot.start((ctx) => {
     ctx.reply(
-      'Keyboard',
+      "Hello " + ctx.from.first_name + ", welcome to the Nym Coindesk bot💰"
+    );
+    ctx.reply(
+      "What would you like to do?",
       Markup.inlineKeyboard([
-        Markup.button.callback('First option', 'first'),
-        Markup.button.callback('Second option', 'second'),
+        Markup.button.callback("Deposit", "deposit"),
+        Markup.button.callback("Withdraw", "withdraw"),
+        Markup.button.callback("Get Advice", "advice"),
       ])
     );
   });
 
-bot.on('text', (ctx) => {
-    console.log('=== 2. text command START');
-  ctx.reply(
-    'You chose the ' +
-      (ctx.message.text === 'first' ? 'First' : 'Second') +
-      ' Option!'
-  );
-  console.log('=== 2. text command END');
-  if (chatId) {
-    telegram.sendMessage(
-      chatId,
-      'This message was sent without your interaction!'
+  bot.help((ctx) => {
+    ctx.reply("Send /start to begin");
+  });
+
+  bot.command("keyboard", (ctx) => {
+    ctx.reply(
+      "Keyboard",
+      Markup.inlineKeyboard([
+        Markup.button.callback("First option", "first"),
+        Markup.button.callback("Second option", "second"),
+      ])
     );
-  }
+  });
+
+  bot.on("text", (ctx) => {
+    console.log("=== 2. text command START");
+    ctx.reply(
+      "You chose the " +
+        (ctx.message.text === "first" ? "First" : "Second") +
+        " Option!"
+    );
+    console.log("=== 2. text command END");
+    if (chatId) {
+      telegram.sendMessage(
+        chatId,
+        "This message was sent without your interaction!"
+      );
+    }
+  });
+
+  console.log("== Bot is Running ==");
+
+  bot.command("quit", (ctx) => {
+    ctx.telegram.leaveChat(ctx.message.chat.id);
+    ctx.leaveChat();
+  });
+
+  bot.launch();
+
+  // Enable graceful stop
+  process.once("SIGINT", () => bot.stop("SIGINT"));
+  process.once("SIGTERM", () => bot.stop("SIGTERM"));
 });
-
-console.log('== Bot is Running ==')
-bot.launch();
-
-// Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
-
-
-
-
